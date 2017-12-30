bcacc00785b8572ce40fdc285581db449c8b5193
andrewdolman <andydolman@gmail.com>
2017-12-30 23:04:01 +0100

    update data, load from monthly zips

--- data-raw/download-data.R
+++ data-raw/download-data.R
@@ -3,13 +3,56 @@ library(tidyverse)
 library(lubridate)
 library(XML)
 
+## Download monthly zip files
+
+url <- "https://www.madavi.de/sensor/data/2017/data-esp8266-1190180-"
+dts <- c(paste0("2017-0", 4:9), paste0("2017-", 10:11))
+
+urls <- paste0(url, dts, ".zip")
+fl.nms <- paste0("data-raw/zip/esp8266-1190180", dts, ".zip")
+#fl.nms <- paste0("data-raw/raw-files/esp8266-11901802017-05-13.csv")
+if (dir.exists("data-raw/zip")==FALSE){
+  dir.create("data-raw/zip")
+}
+
+# download
+plyr::l_ply(seq_along(urls), function(x) download.file(urls[x], fl.nms[x]))
+
+# unzip
+plyr::l_ply(fl.nms, function(x) unzip(x, exdir = "data-raw/raw-files"))
+
+# read files
+
+fl.nms <- list.files("data-raw/raw-files", include.dirs = FALSE)
+
+dat <-  plyr::ldply(fl.nms, function(x)
+  read_delim(x, delim = ";", col_types = paste0("c",
+                                                paste0(rep("n", 15), collapse = ""),
+                                                paste0(rep("i", 4), collapse = ""),
+                                                collapse = ""))) %>%
+  tbl_df()
+
+home.data.mothly.zip <- dat %>%
+  mutate(Time = as.POSIXct(Time),
+         Date = as.Date(Time, ts = "Europe/Berlin")) %>%
+  group_by(Time, Date) %>%
+  select_if(is.numeric) %>%
+  ungroup()
+
+home.data <- bind_rows(luftr::home.data, home.data.new) %>%
+  distinct(.) %>%
+  mutate(Date = as.Date(Time, ts = "Europe/Berlin"),
+         PM_10 = SDS_P1,
+         PM_2.5 = SDS_P2)
+
+
 
 ## Get only new data
 
 
 # Get latest date of existing data
 
-latest.date <- as.Date(max(luftr::home.data$Time))
+latest.date <- as.Date(max(home.data.mothly.zip$Date))
 
 
 # fl.nms <-
@@ -20,7 +63,7 @@ latest.date <- as.Date(max(luftr::home.data$Time))
 # links <- XML::xpathSApply(doc, "//a/@href")
 
 url <- "https://www.madavi.de/sensor/data/data-esp8266-1190180-"
-dts <- seq(latest.date, lubridate::today(), by='days')
+dts <- seq(latest.date+1, lubridate::today(), by='days')
 
 urls <- paste0(url, dts, ".csv")
 fl.nms <- paste0("data-raw/raw-files/esp8266-1190180", dts, ".csv")
@@ -48,11 +91,12 @@ home.data.new <- dat %>%
 
 ## append new data
 
-home.data <- bind_rows(luftr::home.data, home.data.new) %>%
+home.data <- bind_rows(home.data.mothly.zip, home.data.new) %>%
   distinct(.) %>%
   mutate(Date = as.Date(Time, ts = "Europe/Berlin"),
          PM_10 = SDS_P1,
-         PM_2.5 = SDS_P2)
+         PM_2.5 = SDS_P2,
+         Temp = ifelse(is.na(Temp), BMP_temperature, Temp))
 
 home.data.long <- home.data %>%
   select(-SDS_P1, -SDS_P2) %>%
